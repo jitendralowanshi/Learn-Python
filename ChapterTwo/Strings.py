ef1c7766fb02c331c343577adff6866b5e8b2956
jitendra Lowanshi <jitendralowanshi3@gmail.com>
2024-12-22 10:59:32 +0530

    addd another chapter two file

---
+++ ChapterTwo/Strings.py
@@ -0,0 +1,22 @@
+# new lines use in python
+str1 = "this is a string.\nwe are creating it in python."
+str2 = "this is a string.\twe are creating it in python."
+print(str1)
+print(str2 , "\n")
+
+
+# concatinating two strings 
+s1 = "Hello"
+s2 = "World"
+
+final_str = s1 + s2
+print("Concatinating strings are : ",final_str)
+length = len(final_str)  #string length store in variable and print length
+print("length is : ", length,"\n")
+
+
+#this len count with spaces 
+finalString = s1 + " " + s2
+stringlen = len(finalString)
+print("Concatinating string with OneSpace : ",finalString)
+print("Length is : ", stringlen)
